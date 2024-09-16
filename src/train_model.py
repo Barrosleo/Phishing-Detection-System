d0bd75c6c36343f45ad01d82b1daaea59f097d16
Leo <115377071+Barrosleo@users.noreply.github.com>
2024-09-17 00:56:34 +0100

    train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
+import joblib
 
 def train_model(features_path, labels_path):
     features = pd.read_csv(features_path)
@@ -15,5 +16,5 @@ def train_model(features_path, labels_path):
 
 if __name__ == "__main__":
     model = train_model('../data/preprocessed_features.csv', '../data/preprocessed_labels.csv')
-    import joblib
     joblib.dump(model, '../models/phishing_detector.pkl')
+
